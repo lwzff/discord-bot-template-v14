96f5b13b5c1fa91dccab04827693bc1fa7049f12
lwz <34718695+lwzff@users.noreply.github.com>
2024-08-08 21:20:24 +0200

    Hello (test) command for all guilds
    - This command will be deployed on every guilds your bot is on.

--- commands/public/set-logs-channel.js
+++ commands/public/set-logs-channel.js
@@ -1,35 +1,19 @@
 const { Client, SlashCommandBuilder, CommandInteraction } = require("discord.js");
 
-// Requirements
-const settings = require("../../models/settings");
-
 module.exports = {
     developer: false,
     data: new SlashCommandBuilder()
-    .setName("logs-channel")
-    .setDescription("Set the logs channel.")
-    .setDefaultMemberPermissions(0) // 0 = Only users with "Administrator" permissions will be able to run this command, unless you edit the command' permissions in Server Settings -> Integrations.
-    .addChannelOption(option =>
-        option
-            .setName("channel")
-            .addChannelTypes("text")
-            .setRequired(true) 
-    ),
+    .setName("hello")
+    .setDescription("Just wanna say hi!")
+    ,
 
     /**
      * @param { Client } client
      * @param { CommandInteraction } interaction
      */
     async execute (interaction, client) {
-        await interaction.deferReply({ ephemeral: true });
-
-        const channel = interaction.options.getChannel("channel");
-           
-        const findSettings = await settings.findOne({ guildId: interaction.guild.id });
-        if (!findSettings) await settings.create({ guildId: interaction.guild.id, logsChannelId: channel.id });
-        else await findSettings.updateOne({ logsChannelId: channel.id });
-
-        await interaction.editReply({ content: `Logs channel updated to: <@&${channel.id}>.` });
-        
+        await interaction.reply({
+            content: `Hi ${interaction.user} !`
+        });
     }
 }

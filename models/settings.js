883ca889f94c55ef30253a919863026beac606db
lwz <34718695+lwzff@users.noreply.github.com>
2024-08-08 21:15:03 +0200

    Removing model/schema & function
    - I'll put an example .js file of a Mongoose (MongoDB) models/schemas in the wiki/documentation/readme of the repository.
    
    - Same thing for functions.

--- models/settings.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = mongoose.model(
-    "settings",
-    new mongoose.Schema({
-        guildId: String,
-        logsChannelId: String,
-    })
-);
